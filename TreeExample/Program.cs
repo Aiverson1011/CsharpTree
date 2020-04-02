483bde413a30cfe2fabc9339f83df9cf71133ddd
Amanda Iverson <ajohnson764@gmail.com>
2020-04-02 10:50:43 -0700

    Add project files.

---
+++ TreeExample/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TreeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Tree();
+        }
+
+        static void Tree()
+        {
+            // create an integer root
+
+            #region tree construction
+            Tree<int> tree = new Tree<int>(10);
+
+            Node<int> node2 = new Node<int>(11);
+            Node<int> node3 = new Node<int>(12);
+            Node<int> node4 = new Node<int>(13);
+            Node<int> node5 = new Node<int>(14);
+            Node<int> node6 = new Node<int>(15);
+
+            tree.Root.LeftChild = node2;
+            tree.Root.RightChild = node3;
+
+            node2.LeftChild = node4;
+            node2.RightChild = node5;
+
+            node3.LeftChild = node6;
+
+            #endregion
+
+            List<int> pre = tree.PreOrder(tree.Root);
+            Console.WriteLine("=====");
+            Console.WriteLine(String.Join(',', pre));
+
+            Console.WriteLine("INORDER");
+            List<int> inOrder = tree.InOrder(tree.Root);
+            Console.WriteLine(String.Join(',', inOrder));
+
+            Console.WriteLine("=====");
+            Console.WriteLine("POSTORDER");
+            List<int> postOrder =  tree.PostOrder(tree.Root);
+            Console.WriteLine(String.Join(',', postOrder));
+
+
+
+
+        }
+
+    }
+}
---
+++ TreeExample/Tree.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TreeExample
+{
+    class Tree<T>
+    {
+        public Node<T> Root { get; set; }
+
+        public Tree(T value)
+        {
+            Root = new Node<T>(value);
+
+        }
+
+        public List<T> PreOrder(Node<T> node)
+        {
+            List<T> traversal = new List<T>();
+            PreOrder(node, traversal);
+            return traversal;
+
+        }
+
+        private void PreOrder(Node<T> node, List<T> traversal)
+        {
+            traversal.Add(node.Value);
+
+            if (node.LeftChild != null)
+            {
+                PreOrder(node.LeftChild, traversal);
+            }
+
+            if (node.RightChild != null)
+            {
+                PreOrder(node.RightChild, traversal);
+            }
+        }
+
+        public List<T> InOrder(Node<T> node)
+        {
+
+            List<T> traversal = new List<T>();
+            InOrder(node, traversal);
+            return traversal;
+
+        }
+
+        private void InOrder(Node<T> node, List<T> traversal)
+        {
+            if (node.LeftChild != null)
+            {
+                InOrder(node.LeftChild, traversal);
+            }
+
+            traversal.Add(node.Value);
+
+            if (node.RightChild != null)
+            {
+                InOrder(node.RightChild, traversal);
+            }
+        }
+
+        public List<T> PostOrder(Node<T> node)
+        {
+            List<T> traversal = new List<T>();
+            PostOrder(node, traversal);
+            return traversal;
+        }
+
+        private void PostOrder(Node<T> node, List<T> traversal)
+        {
+
+            if (node.LeftChild != null)
+            {
+                PostOrder(node.LeftChild, traversal);
+            }
+
+            if (node.RightChild != null)
+            {
+                PostOrder(node.RightChild, traversal);
+            }
+
+            traversal.Add(node.Value);
+        }
+
+        public void BreadthFirst(Node<T> root)
+        {
+            Queue<Node<T>> breadth = new Queue<Node<T>>();
+
+            breadth.Enqueue(root);
+
+            while (breadth.TryPeek(out root))
+            {
+                Node<T> front = breadth.Dequeue();
+                Console.Write(front.Value);
+
+                if (front.LeftChild != null)
+                {
+                    breadth.Enqueue(front.LeftChild);
+                }
+
+                if (front.RightChild != null)
+                {
+                    breadth.Enqueue(front.RightChild);
+                }
+
+            }
+        }
+    }
+}
